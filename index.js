416f902c03ee7294f324328e3b25401ee5c3db40
gossoudarev <igossoudarev@herzen.spb.ru>
2018-08-09 02:28:48 +0300

    third

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { transform } = require('babel-core');
+const { Script } = require('vm'); // виртуальная машина
+const { createWriteStream: w } = require('fs'); // поток вывода
+const f = 'index.js';
 (async () => {
-  const src = String(await require('fs').promises.readFile('./src/index.js'));
+  const src = String(await require('fs').promises.readFile(`./src/${f}`));
   const { code } = transform(src, {
   	plugins: ["transform-function-bind"]
   });
-  console.log('READY!');
-  eval(code);
+  console.log('VM READY!');
+  // eval(code);
+  const script = new Script(code);
+  script.runInThisContext(); // выполнить
+  w(`./lib/${f}`).write(code); // записать
 })();
